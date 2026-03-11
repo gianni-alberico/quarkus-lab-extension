6f808246423dbc723f2896052c9a32f3e1d7dde4
Gianni Alberico <gianni.alberico@outlook.fr>
2026-03-11 22:25:05 +0100

    feat(deployment): implement build step to record metadata

--- quarkus-lab-extension/deployment/src/main/java/io/github/giannialberico/quarkus/lab/extension/deployment/QuarkusLabExtensionProcessor.java
+++ quarkus-lab-extension/deployment/src/main/java/io/github/giannialberico/quarkus/lab/extension/deployment/QuarkusLabExtensionProcessor.java
@@ -1,14 +1,39 @@
 package io.github.giannialberico.quarkus.lab.extension.deployment;
 
+import io.github.giannialberico.quarkus.lab.extension.runtime.MetadataRecorder;
 import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.annotations.ExecutionTime;
+import io.quarkus.deployment.annotations.Record;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.pkg.builditem.CurateOutcomeBuildItem;
+import io.quarkus.maven.dependency.ResolvedDependency;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collection;
 
 class QuarkusLabExtensionProcessor {
 
     private static final String FEATURE = "quarkus-lab-extension";
+    private static final Logger log = LoggerFactory.getLogger(QuarkusLabExtensionProcessor.class);
 
     @BuildStep
     FeatureBuildItem feature() {
         return new FeatureBuildItem(FEATURE);
+    }
+
+    @Record(ExecutionTime.RUNTIME_INIT)
+    @BuildStep
+    public void registerBuildDateAndDeps(CurateOutcomeBuildItem curateOutcomeBuildItem, MetadataRecorder metadataRecorder) {
+        Collection<ResolvedDependency> deps = curateOutcomeBuildItem.getApplicationModel().getDependencies().
+                stream()
+                .filter(ResolvedDependency::isRuntimeCp)
+                .toList();
+
+        log.info("found {} application dependencies", deps.size());
+        metadataRecorder.printBuildDateAndDeps(
+                System.currentTimeMillis(),
+                deps.stream().map(ResolvedDependency::getArtifactId).toList()
+        );
     }
 }
