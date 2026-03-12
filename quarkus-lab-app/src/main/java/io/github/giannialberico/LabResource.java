2ec4a908254893d1828d2f5b19fabcd9779921fc
Gianni Alberico <gianni.alberico@outlook.fr>
2026-03-12 17:52:49 +0100

    feat(app): add color test endpoint

--- quarkus-lab-app/src/main/java/io/github/giannialberico/LabResource.java
+++ quarkus-lab-app/src/main/java/io/github/giannialberico/LabResource.java
@@ -1,5 +1,8 @@
 package io.github.giannialberico;
 
+import io.github.giannialberico.quarkus.lab.extension.runtime.Colored;
+import io.smallrye.common.annotation.RunOnVirtualThread;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -11,11 +14,22 @@ import java.lang.reflect.Method;
 @Path("/lab")
 public class LabResource {
 
+    @Inject
+    public Colored colored;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/reflect")
     public String reflect() throws InvocationTargetException, IllegalAccessException, ClassNotFoundException, NoSuchMethodException {
         Method method = Thread.currentThread().getContextClassLoader().loadClass("io.github.giannialberico.ReflectiveService").getDeclaredMethod("hello");
         return "Invoked ReflectiveService.hello(): " + method.invoke(new ReflectiveService());
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/color")
+    @RunOnVirtualThread
+    public String color() {
+        return colored.getColor();
     }
 }
