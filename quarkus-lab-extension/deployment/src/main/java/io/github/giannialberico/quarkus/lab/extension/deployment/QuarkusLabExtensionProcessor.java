4925a0009665cedc9724cd045ef8e892782d9d36
Gianni Alberico <gianni.alberico@outlook.fr>
2026-03-12 19:23:18 +0100

    feat(deployment): register synthetic beans with Identifier qualifiers

--- quarkus-lab-extension/deployment/src/main/java/io/github/giannialberico/quarkus/lab/extension/deployment/QuarkusLabExtensionProcessor.java
+++ quarkus-lab-extension/deployment/src/main/java/io/github/giannialberico/quarkus/lab/extension/deployment/QuarkusLabExtensionProcessor.java
@@ -1,9 +1,11 @@
 package io.github.giannialberico.quarkus.lab.extension.deployment;
 
+import io.github.giannialberico.quarkus.lab.extension.runtime.ExtensionBean;
 import io.github.giannialberico.quarkus.lab.extension.runtime.MetadataRecorder;
 import io.github.giannialberico.quarkus.lab.extension.runtime.impl.Blue;
 import io.github.giannialberico.quarkus.lab.extension.runtime.impl.Red;
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
+import io.quarkus.arc.deployment.SyntheticBeanBuildItem;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.annotations.ExecutionTime;
@@ -13,7 +15,9 @@ import io.quarkus.deployment.builditem.FeatureBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 import io.quarkus.deployment.pkg.builditem.CurateOutcomeBuildItem;
 import io.quarkus.maven.dependency.ResolvedDependency;
+import io.smallrye.common.annotation.Identifier;
 import io.smallrye.common.annotation.RunOnVirtualThread;
+import jakarta.inject.Singleton;
 import org.jboss.jandex.ClassInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -74,5 +78,27 @@ class QuarkusLabExtensionProcessor {
 
         log.info("registering {} as Colored implementation", implementation.getName());
         return AdditionalBeanBuildItem.unremovableOf(implementation);
+    }
+
+    @Record(ExecutionTime.STATIC_INIT)
+    @BuildStep
+    public void registerSyntheticBeans(LabBuildTimeConfig labBuildTimeConfig, BuildProducer<SyntheticBeanBuildItem> syntheticBeanProducer, MetadataRecorder metadataRecorder) {
+        labBuildTimeConfig.beans()
+                .ifPresent(beans -> beans.forEach(bean -> {
+                    log.info("registering {} as synthetic bean", bean);
+
+                    syntheticBeanProducer.produce(
+                            SyntheticBeanBuildItem
+                                    .configure(ExtensionBean.class)
+                                    .scope(Singleton.class)
+                                    .unremovable()
+                                    .addQualifier()
+                                        .annotation(Identifier.class)
+                                        .addValue("value", bean)
+                                        .done()
+                                    .supplier(metadataRecorder.extensionBeanSupplier(bean))
+                                    .done()
+                    );
+        }));
     }
 }
