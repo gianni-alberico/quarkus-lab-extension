2641cdb2f8f8f61ae041bb3a2851c75abc65bfe5
Gianni Alberico <gianni.alberico@outlook.fr>
2026-03-12 19:24:47 +0100

    feat(app): add extensionBean test endpoint + bean configuration

--- quarkus-lab-app/src/main/java/io/github/giannialberico/LabResource.java
+++ quarkus-lab-app/src/main/java/io/github/giannialberico/LabResource.java
@@ -1,13 +1,18 @@
 package io.github.giannialberico;
 
 import io.github.giannialberico.quarkus.lab.extension.runtime.Colored;
+import io.github.giannialberico.quarkus.lab.extension.runtime.ExtensionBean;
+import io.quarkus.arc.Arc;
+import io.smallrye.common.annotation.Identifier;
 import io.smallrye.common.annotation.RunOnVirtualThread;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+import java.lang.classfile.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -32,4 +37,14 @@ public class LabResource {
     public String color() {
         return colored.getColor();
     }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/extensionbean/{name}")
+    public String extensionBean(@PathParam("name") String name) {
+        Identifier.Literal identifier = Identifier.Literal.of(name);
+        ExtensionBean extensionBean = Arc.container().select(ExtensionBean.class, identifier).get();
+        return extensionBean.greet();
+    }
+
 }
