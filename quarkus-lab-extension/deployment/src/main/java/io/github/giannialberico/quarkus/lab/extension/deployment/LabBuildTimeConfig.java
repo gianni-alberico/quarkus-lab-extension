6475d626f354b5161301160f76d592eea32ad7e7
Gianni Alberico <gianni.alberico@outlook.fr>
2026-03-12 19:21:19 +0100

    feat(deployment): add LabBuildTimeConfig for bean naming

---
+++ quarkus-lab-extension/deployment/src/main/java/io/github/giannialberico/quarkus/lab/extension/deployment/LabBuildTimeConfig.java
@@ -0,0 +1,27 @@
+package io.github.giannialberico.quarkus.lab.extension.deployment;
+
+import io.quarkus.runtime.annotations.ConfigPhase;
+import io.quarkus.runtime.annotations.ConfigRoot;
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithDefault;
+
+import java.util.List;
+import java.util.Optional;
+
+@ConfigMapping(prefix = "quarkus.lab-extension")
+@ConfigRoot(phase = ConfigPhase.BUILD_TIME)
+public interface LabBuildTimeConfig {
+
+    /**
+     * List of named beans to create synthetically at build time.
+     * <p>
+     * Each entry in the list will result in a synthetic CDI bean named with {@code @Identifier},
+     * injectable in the application using {@code @Identifier("name")}.
+     * </p>
+     * Example:
+     * <pre>
+     *     quarkus.lab-extension.beans=name,name2
+     * </pre>
+     */
+    Optional<List<String>> beans();
+}
